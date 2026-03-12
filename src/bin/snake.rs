ccc859f9d31ee058b81d557c6b5e7749d7da3f05
Jed Nicholson <jed.nicholson@durham.ac.uk>
2026-03-12 16:57:59 +0000

    added rand crate, implemented fruit and prevented snake from chaning dir to opposite of current dir

--- src/bin/snake.rs
+++ src/bin/snake.rs
@@ -2,13 +2,14 @@
 use std::collections::VecDeque;
 use std::io::{stdout, Write};
 use std::ops::Add;
+use std::cmp::Eq;
 use std::thread;
 use std::time::{self, Duration};
-use crossterm::event::KeyCode;
 use crossterm::{ExecutableCommand, execute,
-    cursor::{RestorePosition, SavePosition, Hide, Show, MoveTo}, 
-    terminal::{enable_raw_mode, disable_raw_mode, Clear, ClearType::All},
-    event::{poll, read, Event}};
+                cursor::{RestorePosition, SavePosition, Hide, Show, MoveTo}, 
+                terminal::{enable_raw_mode, disable_raw_mode, Clear, ClearType::All},
+                event::{poll, read, Event, KeyCode}};
+use rand::RngExt;
 
 const BOARD_SIZE: (usize, usize) = (12, 12);
 #[derive(Clone, Debug, Copy)]
@@ -22,6 +23,7 @@ impl Coord {
     pub const RIGHT: Coord = Coord {x:1,y:0};
     pub const UP: Coord = Coord {x:0,y:-1};
     pub const LEFT: Coord = Coord {x:-1,y:0};
+    pub const ZERO: Coord = Coord {x:0, y:0};
 
     fn index(self) -> usize{
         (self.x + self.y * (BOARD_SIZE.1) as i16) as usize
@@ -35,6 +37,14 @@ impl Add for Coord {
         Self {x: self.x+rhs.x, y: self.y+rhs.y}
     }
 }
+
+impl PartialEq for Coord {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Eq for Coord {}
 
 struct Snake {
     body: VecDeque<Coord>,
@@ -62,6 +72,11 @@ impl Snake {
         if self.board[new_head.index()] == '#' {
             return Err("Collision")
         }
+        else if self.board[new_head.index()] == 'X' {
+            self.board[new_head.index()] = '@';
+            spawn_fruit(self);
+            return Ok(())
+        }
         self.board[new_head.index()] = '@';
 
         let tail = self.body.front().expect("no body");
@@ -83,7 +98,7 @@ fn print_board(board: &[char]){
 }
 fn main () -> std::io::Result<()>{
     enable_raw_mode()?;
-    execute!(stdout(), Hide, Clear(All), MoveTo(0,0))?;
+    execute!(stdout(), Hide, Clear(All), MoveTo(0,5))?;
     let board: [char;BOARD_SIZE.0 * BOARD_SIZE.1] = 
                                 ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
                                  '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',
@@ -100,8 +115,8 @@ fn main () -> std::io::Result<()>{
 
     let mut body: VecDeque<Coord> = VecDeque::new();
     body.push_back(Coord { x: 1, y: 1 });
-    body.push_back(Coord { x: 2, y: 1 });
     let mut snake: Snake = Snake::new(body, Coord::RIGHT, board);
+    spawn_fruit(&mut snake);
     loop {
         thread::sleep(time::Duration::from_millis(300));
         match snake_loop(&mut snake) {
@@ -134,18 +149,43 @@ fn change_dir(snake: &mut Snake)-> std::io::Result<()> {
     while poll(Duration::from_secs(0))? == true {
         event = Some(read()?);
     }
-
+    let mut dir: Coord = Coord::ZERO;
     match event {
         Some(Event::Key(key)) => {
             match key.code {
-                KeyCode::Up => snake.dir = Coord::UP,
-                KeyCode::Down => snake.dir = Coord::DOWN,
-                KeyCode::Left => snake.dir = Coord::LEFT,
-                KeyCode::Right => snake.dir = Coord::RIGHT,
+                KeyCode::Up => dir = Coord::UP,
+                KeyCode::Down => dir = Coord::DOWN,
+                KeyCode::Left => dir = Coord::LEFT,
+                KeyCode::Right => dir = Coord::RIGHT,
                 _ => ()
             }
         }
         _ => ()
     }
+    if dir + snake.dir == Coord::ZERO {
+        return Ok(())
+    }
+    if dir != Coord::ZERO {
+        snake.dir=dir
+    }
     Ok(())
+}
+
+fn spawn_fruit(snake: &mut Snake) {
+    let mut rng = rand::rng();
+    let mut pos: usize;
+    'gen_pos: loop {
+        pos = rng.random_range(13..131);
+        if pos % 12 == 0 || pos % 12 == 11{
+            continue
+        }
+        for i in 0..snake.body.len() {
+            if snake.body[i].index() == pos {
+                continue 'gen_pos;
+            }
+        }
+        break
+
+    }
+    snake.board[pos] = 'X';
 }
